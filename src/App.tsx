b8f38d64a69efb340da4b59b6b9678b9d41ff77c
bayromest <bayramtrabelsi.bt@gmail.com>
2024-08-23 17:18:38 +0100

    add image

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ const App = () => {
     <div className="p-12 flex flex-col items-center bg-black text-white text-center w-96">
       <img src="/icons/icon128.png" className="" />
       <h1 className="text-xl font-bold">Inspiration should be free</h1>
-      <p className="text-xs text-gray-300">Developped by Bayrem</p>
+      <div className="text-xs text-gray-400 flex space-x-1 items-center">
+        <p>Developped by</p>
+        <img src="/images/bayrem.jpg" className="h-4 rounded-full" />
+        <p>Bayrem</p>
+      </div>
       <a
         href="https://github.com/bayromest/unblur-mobbin"
         target="_blank"
